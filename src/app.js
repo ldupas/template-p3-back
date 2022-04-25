23a7cde46a393373bdeffcd45876c5ee0ba3da16
louisdupas <louisdupas_pro@pm.me>
2022-04-25 09:15:49 +0200

    deleted spacing

--- src/app.js
+++ src/app.js
@@ -17,16 +17,12 @@ connection.connect((err) => {
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
 app.use(cors())
 app.use('/api', router);
-
 
 app.get("/", (req, res) => {
     res.send("Welcome");
 });
-
-
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
