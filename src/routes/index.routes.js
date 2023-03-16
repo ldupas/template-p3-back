de3574d0b1e425129f499a3e7575bf2839b777a9
Louis Dupas <52036445+ldupas@users.noreply.github.com>
2023-03-16 16:47:35 +0100

    Update index.routes.js

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,9 +1,7 @@
 const router = require('express').Router();
-const usersRouter = require('./user.routes');
 const animalsRouter = require('./animal.routes');
 const productsRouter = require('./product.routes');
 
-router.use('/users', usersRouter);
 router.use ('/animals', animalsRouter);
 router.use('/products', productsRouter);
 
